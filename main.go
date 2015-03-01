c9ec8198140056e1026e430b26c2762b388fd830
Hagen Mahnke <hagenmahnke@googlemail.com>
2015-03-01 14:45:00 +0100

    sleep between calculations
    for readability of output on comsole

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
+import "time"
 
 func main() {
+	delay := time.Duration(3) * time.Second
 	board := initializeBoard(5)
 	board.print()
 	for {
 		(&board).advance()
 		board.print()
+		time.Sleep(delay)
 	}
 }
